761a1de669aa4c30c9c53878174fba9614d10c60
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-01-31 09:55:53 +0100

    Start on week1 assignments and exercises

---
+++ Exercises/OneOneFourteen.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+public class OneOneFourteen {
+    public int logMethod(int N) {
+        return N;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
+
---
+++ KattisAssignments/DisjunkteMaengder.java
@@ -0,0 +1,21 @@
+package KattisAssignments;
+
+import java.util.Scanner;
+
+// import edu.princeton.cs.algs4.UF;
+// import edu.princeton.cs.algs4.StdIn;
+// import edu.princeton.cs.algs4.StdOut;
+
+public class DisjunkteMaengder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+    
+        for (int i = 0; i < m; i++) {
+            
+        }
+
+        sc.close();
+    }
+}
