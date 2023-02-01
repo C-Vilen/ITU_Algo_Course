f69fd3effd0b68b051e1af42ac58c90b337b208e
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-02-01 10:53:40 +0100

    First attempt of Disjoint kattis

--- Exercises/OneFiveTwo.java
+++ Exercises/OneFiveTwo.java
@@ -2,6 +2,17 @@ package Exercises;
 
 public class OneFiveTwo {
     
+    // Give p and q the same root
+    public void union(int p, int q) { 
+    int pRoot = find(p);
+    int qRoot = find(q);
+    if (pRoot == qRoot) return;
+    id[pRoot] = qRoot;
+    count--;
+    }
+
+
+
 
     public static void main(String[] args) {
         
--- KattisAssignments/DisjointSets/DisjointSets.java
+++ KattisAssignments/DisjointSets/DisjointSets.java
@@ -1,20 +1,55 @@
 package KattisAssignments.DisjointSets;
 
 import edu.princeton.cs.algs4.UF;
-import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+// import edu.princeton.cs.algs4.StdIn;
+// import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Scanner;
 
 public class DisjointSets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        for (int i = 0; i < m; i++) {
-            
-        }
+        try {
+            // Reads the amount of singletons & following amount of lines & initiates UnionFind class 
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            UF UnionFind = new UF(n);
+            for (int i = 0; i < m; i++) {
+                
+                // Reads the input operation
+                int operation = sc.nextInt();
+                // Reads the node of p
+                int p = sc.nextInt();
+                // Reads the node of q
+                int q = sc.nextInt();
 
-        sc.close();
+                // Executing the query command by checking if p & q is connected
+                if (operation == 0) {
+                    boolean ConnectedQuery = UnionFind.find(p) == UnionFind.find(q);
+                    if (ConnectedQuery) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                } 
+                
+                // Executing the union command by connecting p & q
+                if (operation == 1) {
+                    UnionFind.union(p, q);
+                }
+
+                // Executing the move command by moving the p into the q set
+                if (operation == 2) {
+                    boolean ConnectedQuery = UnionFind.find(p) == UnionFind.find(q);
+                    if (ConnectedQuery) {
+                        return;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+            sc.close();
+        }
     }
 }
