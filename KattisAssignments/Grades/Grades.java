f724136acb269382044084b7dcf86fcfad9da296
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-02-06 20:21:38 +0100

    Structure for grades algo assignment

---
+++ KattisAssignments/Grades/Grades.java
@@ -0,0 +1,10 @@
+package KattisAssignments.Grades;
+
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Insertion;
+
+public class Grades {
+    
+}
