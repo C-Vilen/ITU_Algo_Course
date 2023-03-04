258187cdb6306defae0773b59778921fe1f10317
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-03-04 13:26:06 +0100

    Added templates for Flights and Harmony & Conflict

---
+++ KattisAssignments/Flights/Flights.java
@@ -0,0 +1,10 @@
+package KattisAssignments.Flights;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Flights {
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        int m = StdIn.readInt();
+    }
+}
---
+++ KattisAssignments/HarmonyAndConflict/HarmonyAndConflict.java
@@ -0,0 +1,15 @@
+package KattisAssignments.HarmonyAndConflict;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class HarmonyAndConflict {
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        int m = StdIn.readInt();
+        for (int i = 0; i < m; i++) {
+            int one = StdIn.readInt();
+            int two = StdIn.readInt();
+            int three = StdIn.readInt();
+        }
+    }
+}
